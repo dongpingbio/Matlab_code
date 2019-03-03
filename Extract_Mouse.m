767db14f2aedd3c5ec387bee3360d7da76f0efe5
dongpingbio <dongpingbio@hotmail.com>
2019-03-02 21:00:50 -0500

    Nature_Neuroscience_paper_Mice_tracing
    2019-03-02 uploaded by Ping Dong.
    Any question please contact dongping@zju.edu.cn

--- Extract_Mouse.m
+++ Extract_Mouse.m
@@ -1,33 +1,25 @@
-function c_pos=Extract_diff_back(background_frame,input_frame,xLeft, xRight, yUp, yDown,area_L,area_H)
+function c_pos=Extract_Mouse(input_frame,xLeft, xRight, yUp, yDown,area_L,area_H)
 % To chop the ori_image based on analysis area
 
-background_frame_a=background_frame(yUp:yDown,xLeft:xRight,:);
 input_frame_a=input_frame(yUp:yDown,xLeft:xRight,:);
 
 % Import ori
-I0_b=background_frame_a;
 I0_m=input_frame_a;
 % figure(2);
 % imshow(I0_m);
 
 % RGB to Grey
-I0_b_g=rgb2gray(I0_b);
-% figure(3);
-% imshow(I0_b_g);
+
+
 I0_m_g=rgb2gray(I0_m);
 % figure(4);
 % imshow(I0_m_g);
 
-% substract the image to the background
-I_s=imsubtract(I0_b_g,I0_m_g);
-% figure(5);
-% imshow(I_s);
-
-I_s_BW = im2bw(I_s, 0.2); % Gray to BW,  to be set
+I_s_BW = ~im2bw(I0_m_g, 0.2); % Gray to BW,  to be set
 % figure(6);
 % imshow(I_s_BW);
 
-I_s_BW_m = medfilt2(I_s_BW,[10,10]); % Medium Filter, get rid of pepper noise
+I_s_BW_m = medfilt2(I_s_BW,[5,5]); % Medium Filter, get rid of pepper noise
 % figure(7);
 % imshow(I_s_BW_m);
 
@@ -48,9 +40,11 @@ C1 = reshape(C1, 2, length(C1)/2)';
 c_pos=C1;
 
 % Mark the connected region on the orignal picture
-
-% figure(2); hold on;
-% plot(C1(:,1), C1(:,2), 'g+', 'MarkerSize', 10);
+% 
+% figure(3);
+% imshow(I0_m);
+% hold on;
+% plot(C1(:,1), C1(:,2), 'r+', 'MarkerSize', 10);
 % hold off;
 
 end
--- Mice_trajectory.m
+++ Mice_trajectory.m
@@ -1,12 +1,13 @@
-close all;
-clear all;
-clc;
+function Mice_trajectory(movie_input_filename)
+% close all;
+% clear all;
+% clc;
 
 % This program is used to analysis the animal trajectory based on the video
 % taken previewously
 
 % Load the movie
-filename='4#_Video_flight.mov';
+filename=movie_input_filename;
 
 % Load movie_raw_data
 E_M_movie = VideoReader(filename,'Tag','My reader object');
@@ -23,16 +24,38 @@ background_frame=I_background;
 hf = figure(1);
 set(hf, 'position', [150 150 vidWidth vidHeight])
 imshow(I_background);
-[a_xLeft, a_xRight, a_yUp, a_yDown,a_x,a_y,a_w,a_h]=ManualDraw_analysisArea;
+% [a_xLeft, a_xRight, a_yUp, a_yDown,a_x,a_y,a_w,a_h]=ManualDraw_analysisArea;
+% variation only for multi-operation
+
+
+% % for left
+% a_xLeft=130;
+% a_xRight=431;
+% a_yUp=96;
+% a_yDown=396;
+% a_x=130;
+% a_y=96;
+% a_w=301;
+% a_h=300;
+
+
+% for right
+a_xLeft=445;
+a_xRight=735;
+a_yUp=99;
+a_yDown=391;
+a_x=445;
+a_y=99;
+a_w=290;
+a_h=292;
 
 % Manual input the length of drawed line
-Ruler_p=ManualDraw_Ruler();
-draw_length=input('Please enter the length of the Line (cm)£º');
+% Ruler_p=ManualDraw_Ruler();
+Ruler_p=300;
+% draw_length=input('Please enter the length of the Line (cm)');
+draw_length=45;
 % Calculate the length per pixels
 len_per_pixels=draw_length/Ruler_p;
-
-
-
 
 area_L=100;
 area_H=10000;
@@ -44,17 +67,19 @@ c_pos_sum=[];
 p_dis_sum=0;
 p_vel=0;
 
-framerate=7;
+framerate=E_M_movie.FrameRate;
 time_interval_per_frame=1/framerate; % 24 frame per second
 frame_sample_interval=1; % analysis sample interval
 time_interval_per_sample_frame=time_interval_per_frame*frame_sample_interval;
 
-first_analysis_frame=20;
+first_analysis_frame=720; %start time 30s*24frames/s
 pos_sum=[];
 pos_result_tmp=[];
 mice_appear_frame=0;
-for idx = first_analysis_frame:frame_sample_interval:nFrames
-    
+
+% for idx = first_analysis_frame:frame_sample_interval:nFrames
+nFrames=first_analysis_frame+24*60*15; % analysis for 15 min
+ for idx = first_analysis_frame:frame_sample_interval:nFrames   
     mov(idx).cdata = read(E_M_movie,idx);
     input_frame = mov(idx).cdata;
     % Get the central point of the mouse
@@ -100,7 +125,7 @@ for idx = first_analysis_frame:frame_sample_interval:nFrames
     % plot the central point and the trajectory of the mouse
     rectangle('Position',[a_x,a_y,a_w,a_h],'EdgeColor','k','LineWidth',2);
     plot(mice_x,mice_y, 'g+', 'MarkerSize', 10);
-    plot(c_pos_sum(:,1),c_pos_sum(:,2),'r');
+    plot(c_pos_sum(:,1),c_pos_sum(:,2),'r','LineWidth',2);
     if idx==first_analysis_frame
         disp('begin')
     else
@@ -109,6 +134,8 @@ for idx = first_analysis_frame:frame_sample_interval:nFrames
     hold off;
     
 end
+trace_fig_name=['trace_',filename(1:end-4)];
+saveas(gcf,trace_fig_name,'tif');
 
 % Calculate averange speed per second
 nlen=length(r_vel);
@@ -118,6 +145,8 @@ for i=1:ntime
 end
 figure(03)
 plot(aver_vel)
+vel_fig_name=['vel_',filename(1:end-4)];
+saveas(gcf,vel_fig_name,'tif')
 vel_file_name=['Speed_',filename(1:end-4),'.xls'];
 xlswrite(vel_file_name,aver_vel');
 
